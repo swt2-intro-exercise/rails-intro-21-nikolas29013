b20fac88d2fc8f5d470708d736db4495df7b5806
Nikolas <nikolas.hoellerl@student.hpi.uni-potsdam.de>
2021-11-27 17:21:16 +0100

    modified:   app/models/author.rb

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,10 @@
 class Author < ApplicationRecord
+  def initialize(first_name,last_name,homepage)
+    @first_name = first_name
+    @last_name = last_name
+    @homepage = homepage 
+  end
   def name
-    return author.first_name + author.laste_name
+    puts first_name + last_name
   end
 end
