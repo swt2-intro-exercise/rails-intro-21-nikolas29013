869c57af8c503d279bffa208c4181397cb17fbd7
Nikolas <nikolas.hoellerl@student.hpi.uni-potsdam.de>
2021-11-27 17:32:48 +0100

    modified:   app/controllers/authors_controller.rb 	modified:   app/models/author.rb

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,7 @@
 class AuthorsController < ApplicationController
    def new
+      @author = Author.new(params[:author])
+      @author.save
+      redirect_to @author
    end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,2 @@
 class Author < ApplicationRecord
-  def initialize(author)
-    @author = author
-  end
-  def name
-    puts author.first_name + author.last_name
-  end
 end
