739fcc3aa7b7fafe3d48e9d35474c13bd4a5d259
Nikolas <nikolas.hoellerl@student.hpi.uni-potsdam.de>
2021-11-28 14:39:11 +0100

    modified:   app/controllers/authors_controller.rb 	new file:   app/views/authors/edit.html.erb 	modified:   app/views/authors/index.html.erb 	modified:   app/views/authors/show.html.erb

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,12 +11,31 @@ class AuthorsController < ApplicationController
      @author = Author.new
    end
    
+   def edit
+     @author = Author.find(params[:id])
+   end
+   
    def create
-      @author = Author.new(params.require(:author).permit(:first_name,:last_name,:homepage))
+      @author = Author.new(author_params)
       if @author.save
         redirect_to @author
       else
         render 'new'
       end
    end
+   
+  def update
+    @author = Author.find(params[:id])
+ 
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+    end
 end
