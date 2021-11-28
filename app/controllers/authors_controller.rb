ced22022942c625f203d864dbb59f4cddbdcc801
Nikolas <nikolas.hoellerl@student.hpi.uni-potsdam.de>
2021-11-28 14:50:22 +0100

    modified:   app/controllers/authors_controller.rb 	modified:   app/views/authors/index.html.erb

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,5 @@
 class AuthorsController < ApplicationController
+   
    def index
       @authors = Author.all
    end
@@ -33,6 +34,14 @@ class AuthorsController < ApplicationController
       render 'edit'
     end
   end
+  
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+      
+    redirect_to authors_path
+  end
+   
   
   private
     def author_params
