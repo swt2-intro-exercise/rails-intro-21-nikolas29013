7aab287462eec3e857ecc2bcd2b8762f28f8b801
Nikolas <nikolas.hoellerl@student.hpi.uni-potsdam.de>
2021-11-28 16:20:28 +0100

    modified:   app/models/author.rb 	modified:   app/models/paper.rb

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,6 @@ class Author < ApplicationRecord
   def name
     return 'Alan Turing'
   end
+  belongs_to :paper
   validates :last_name, presence: true
 end
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,6 +1,8 @@
 class Paper < ApplicationRecord
+  has_many :comments
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, presence:true,
   		   numericality: true
+  		  
 end
